cfe90be77095f987448bfbf14b63b979da1f14d0
Huy Nguyen <huy_nguyen3105@Anonymous.local>
2019-04-20 12:58:48 +0700

    initial commit

---
+++ Session 1/Serious2.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+user = {
+        'huy':{
+                'name' : 'Nguyen Quang Huy',
+                'age' : 29
+        },
+        'tuananh':{
+                'name' : 'Huynh Tuan Anh',
+                'age' : 22
+        }
+}
+
+@app.route('/user/<username>')
+def index(username):
+    for i in user.keys():
+        if username == i:
+            user_result = user
+        else:
+            user_result = "User not found"
+    return render_template('Serious2.html', user_result = user_result)
+
+if __name__ == '__main__':
+  app.run(debug=True)
+ 
